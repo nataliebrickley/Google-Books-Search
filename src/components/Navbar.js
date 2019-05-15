8ac40b4ad44aa5c2ae32b99961eb97a33627c52d
Natalie <natt_rose@yahoo.com>
2019-05-15 10:38:15 -0700

    added bootstrap, navbar, and assets folder

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from "react"
-
+import "../assets/navbar.css"
 class Navbar extends React.Component {
     render() {
-        return(
-            <div>navbar goes here</div>
+        return (
+            <nav class="navbar">
+                <span class="navbar-brand mb-0 h1">Google Books Search</span>
+            </nav>
         )
     }
 };
