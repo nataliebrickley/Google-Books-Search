22db15e8e7454f04d9cefc02a2aa7fc8b07fe8fc
Natalie <natt_rose@yahoo.com>
2019-05-16 13:45:26 -0700

    added utils folder, installed axios

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,19 @@
 import React from "react"
 import "../assets/navbar.css"
-import {Link} from "react-router-dom";
-  
+import { Link } from "react-router-dom";
+
 class Navbar extends React.Component {
     render() {
         return (
-         
+
             <nav className="navbar fixed-top">
                 <span className="navbar-brand mb-0 h1">Google Books Search</span>
-                <Link to = "/saved">Saved</Link>
+                <div>
+                    <Link to="/">Search</Link>
+                    <Link to="/saved">Saved</Link>
+                </div>
             </nav>
-           
+
         )
     }
 };
